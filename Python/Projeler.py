9fa2a2d16cf1fbd612ca16bcec67f7f343a2fefe
Evren <evrensercan@outlook.com>
2024-12-19 19:17:21 +0300

    ornek

--- Python/Projeler.py
+++ Python/Projeler.py
@@ -72,37 +72,11 @@ Bitir
 
 #############################################################################
 
-"""#py Vize-Final Ortalaması
-vze = input("Vize notunuzu girin :")
-fnl = input("Final notunuzu girin :")
 
-ort = (int(vze)*0.4) + (int(fnl)*0.6)
-
-print(ort)
-
-
-"""
-
-
-"""#py Dairenin çapını ve alanını bulma
-
-"""
 
 
 """#py 2 sayı 4işlem
-ilk = input("İlk sayıyı girin :")
-iki = input("İkinci sayıyı girin :")
 
-
-toplam=int(ilk)+int(iki)
-fark=int(ilk)-int(iki)
-çarpım=int(ilk)*int(iki)
-bölüm=int(ilk)/int(iki)
-
-print("İki Sayının Taplamı :",toplam)
-print("İki Sayının Farkı :",fark)
-print("İki Sayının Çarpımı :",çarpım)
-print("İki Sayının Bölümü :",bölüm)
 """
 
 
---
+++ Python/iki_sayi_dört_islem.py
@@ -0,0 +1,14 @@
+#İki Sayı Dört İşlem
+ilk = input("İlk sayıyı girin :")
+iki = input("İkinci sayıyı girin :")
+
+
+toplam=int(ilk)+int(iki)
+fark=int(ilk)-int(iki)
+çarpım=int(ilk)*int(iki)
+bölüm=int(ilk)/int(iki)
+
+print("İki Sayının Taplamı :",toplam)
+print("İki Sayının Farkı :",fark)
+print("İki Sayının Çarpımı :",çarpım)
+print("İki Sayının Bölümü :",bölüm)
