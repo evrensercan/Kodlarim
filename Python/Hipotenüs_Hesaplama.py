46b39a6555831f585283ab4e1535cfc1d2f21971
Evren <evrensercan@outlook.com>
2024-12-19 18:54:47 +0300

    duzeltme

--- Python/Hipotenüs_Hesaplama.py
+++ Python/Hipotenüs_Hesaplama.py
@@ -1,14 +1,8 @@
 #Üçgenin hipotenüsünü bulma
 a = input("Üçgenin a kenarını girin :")
-
-
 b = input("Üçgenin b kenarını girin :")
 
-
 ckre = (int(a)**2 + int(b)**2)
-
-
 c = ckre**0.5
-
 
 print("Üçgenin c kenarı :" , c )
