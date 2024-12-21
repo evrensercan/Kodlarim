8dd406d49995082a2c0ae573a39f3fd2b3aeb542
Evren <evrensercan@outlook.com>
2024-12-21 14:53:24 +0300

    ekleme

--- Python/AyakkabıNumarasıKontrolSistemi.py
+++ Python/AyakkabıNumarasıKontrolSistemi.py
@@ -2,7 +2,21 @@
 magaza = ['39','40','41','43','44','45']
 nmr = input("Almak İstediğiniz Ayakkabı Numarasını Girin :")
 
+
 if nmr in magaza:
-    print("Seçtiğiniz Beden Mevcuttur.")
+    
+    if nmr==39:
+        print("Seçtiğiniz Ürünün Fiyatı 500TL ")
+    if nmr==40: 
+        print("Seçtiğiniz Ürünün Fiyatı 550TL ")
+    if nmr==41: 
+        print("Seçtiğiniz Ürünün Fiyatı 600TL ")
+    if nmr==43: 
+        print("Seçtiğiniz Ürünün Fiyatı 700TL ")
+    if nmr==44: 
+        print("Seçtiğiniz Ürünün Fiyatı 750TL ")
+    else : 
+        print("Seçtiğiniz Ürünün Fiyatı 800TL ")
+
 else:
     print("Seçtiğiniz Beden Tükenmiştir. ")
