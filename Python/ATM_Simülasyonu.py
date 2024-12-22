10eb1f8b5d87fd517c67cba01a9fd660ac1a2ece
Evren <evrensercan@outlook.com>
2024-12-22 20:03:25 +0300

    atm2

--- Python/ATM_Simülasyonu.py
+++ Python/ATM_Simülasyonu.py
@@ -2,26 +2,36 @@
 
 baslangic_bakiyesi = 7500
 
-print("\n\n\n Hoşgeldiniz \n \t1.Bakiye Sorgulama \n \t2.Para Çekme \n \t3.Para Yatırma")
-karar=input("Lütfen yapmak istediğiniz işlemi seçin : ")
+sifre=input("\n\n\nLütfen Dört Haneli Şifreyi Girin : ")
+if sifre=='1234':
+    print(" Hoşgeldiniz \n \t1.Bakiye Sorgulama \n \t2.Para Çekme \n \t3.Para Yatırma")
+    karar=input("Lütfen yapmak istediğiniz işlemi seçin : ")
 
 
-if karar=='1':
-    print("Şuanki Hesap Bakiyeniz ==>",baslangic_bakiyesi,"TL")
-    
-    
-    
-elif karar=='2':
-    cekme=int(input("Çekmek İstediğiniz Tutarı Girin : "))
-    guncel_bakiye=(baslangic_bakiyesi-cekme)
-    print("Hesabınızdaki Güncel Bakiye ",guncel_bakiye)
+    if karar=='1':
+        print("\tŞuanki Hesap Bakiyeniz ==>",baslangic_bakiyesi,"TL")
 
+
+
+    elif karar=='2':
+        cekme=int(input("Çekmek İstediğiniz Tutarı Girin : "))
+        if cekme>baslangic_bakiyesi:
+            print("\tBakiyeniz Yetersiz")
+        else:
+            guncel_bakiye=(baslangic_bakiyesi-cekme)
+            print("\tHesabınızdaki Güncel Bakiye ",guncel_bakiye)
+
+
+
+    elif karar=='3':
+        ekleme=int(input("Yatırmak İstediğiniz Tutarı Girin : "))
+        guncel_bakiye=(baslangic_bakiyesi+ekleme)
+        print("\tHesabınızdaki Güncel Bakiye ",guncel_bakiye)
+
+
+    else:
+        print("\tGeçersiz işlem")
 
 
 else:
-    ekleme=int(input("Yatırmak İstediğiniz Tutarı Girin : "))
-    guncel_bakiye=(baslangic_bakiyesi+ekleme)
-    print("Hesabınızdaki Güncel Bakiye ",guncel_bakiye)
-
-
-
+    print("Yanlış Şifre")
