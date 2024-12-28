7cbfb4f797c3abf9f32b55a964b7965933b7c26d
Evren <evrensercan@outlook.com>
2024-12-28 17:40:04 +0300

    düzelme

---
+++ Python/Ayakkabı_Kontrol_Sistemi.py
@@ -0,0 +1,24 @@
+#AyakkabıNumarasıKontrolSistemi
+magaza = {
+    '39' : 500,
+    '40' : 600,
+    '41' : 700,
+    '43' : 800,
+    '44' : 900,
+    '45' : 1000
+    }
+nmr = input("\nAlmak İstediğiniz Ayakkabı Numarasını Girin :") 
+
+
+if nmr in magaza: 
+    fiyat=magaza[nmr]
+    print("Seçtiğiniz Ürünün Fiyatı: ",fiyat,"TL")
+         
+    karar = input("Sepete Eklensinmi? (Evet/Hayır) :").lower() 
+    if karar=='evet':
+        print("Ürün Sepetinize Eklenmiştir.")
+    else:
+        magaza.remove(nmr)  
+        print("Diğer ürünlere gözatın ",magaza)
+else: 
+    print("Seçtiğiniz Beden Tükenmiştir.\nMevcut ürünlere gözatın: ",magaza) 
